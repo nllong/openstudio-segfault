d13dec6f2722328f211a14ce7930c4d84cb74456
Dan Macumber <dan.macumber@gmail.com>
2015-11-04 22:54:02 -0700

    Update to fix test

--- timetest.rb
+++ timetest.rb
@@ -1,38 +1,12 @@
 require 'time'
 require 'openstudio'
-require 'parallel'
- 
-puts 'creating threads'
-threads = []
- 
- 
-# (1..100).each do |i|
-#   threads << Thread.new do
-#     # puts Time.new.class
-#     #puts OpenStudio::DateTime.new.class
-#     require_relative 'stupid_measure'
 
-#     a = StupidMeasure.new
-#     puts a.call_me_now(i)
+OpenStudio::Logger::instance().standardOutLogger().enable()
 
-#     #fail "NOT OPENSTUDIO CLASS" unless period_end_date === OpenStudio::Date
-#   end
-# end
-# threads.each(&:join)
-
-
-#Parallel.each((1..64), in_processes: 4) do |i|
 (1..6000).each do |i|
-  #threads << Thread.new do
-    # puts Time.new.class
-    #puts OpenStudio::DateTime.new.class
-    require_relative 'stupid_measure'
+  require_relative 'stupid_measure'
 
-    a = StupidMeasure.new
-    puts a.call_me_now(i)
-
-    #fail "NOT OPENSTUDIO CLASS" unless period_end_date === OpenStudio::Date
-  #end
+  a = StupidMeasure.new
+  puts a.call_me_now(i)
+  GC.start
 end
-
-#threads.each(&:join)
