ee640a3238a34c22e3d31349cac1a203f1c6c365
Dan Macumber <dan.macumber@gmail.com>
2015-11-05 09:43:05 -0700

    Adding object id in case that is getting repeated or something

--- stupid_measure.rb
+++ stupid_measure.rb
@@ -6,6 +6,7 @@ class StupidMeasure
 		from_date = Time.iso8601(time_from).strftime("%Y%m%dT%H%M%S")
     puts "[#{i}] #{from_date}"
     period_start_date = OpenStudio::DateTime.fromISO8601(from_date).get.date
+    puts "[#{i}] #{period_start_date.object_id}"
     puts "[#{i}] #{period_start_date.year}:#{period_start_date.monthOfYear.value}.#{period_start_date.dayOfMonth}"
     puts "[#{i}] #{period_start_date.year}:#{period_start_date.monthOfYear.value}.#{period_start_date.dayOfMonth}"
     puts "[#{i}] #{period_start_date.year}:#{period_start_date.monthOfYear.value}.#{period_start_date.dayOfMonth}"
